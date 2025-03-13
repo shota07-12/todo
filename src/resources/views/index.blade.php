5746bbad18da68fa1a5e4aec0b9132de84529241
shota <ya07masho12@gmail.com>
2025-03-13 17:10:59 +0900

    Update index.blade.php: Add content field and display

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -5,7 +5,6 @@
 <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}">
 @endsection
 
-
 @section('content')
 @if(session('success'))
 <div class="alert alert-success">
@@ -17,26 +16,39 @@
     <form action="{{ route('todos.store') }}" method="POST">
         @csrf
         <div class="create-form__item">
-            <input class="create-form__item-input" type="text" name="title">
+            <input class="create-form__item-input" type="text" name="title" placeholder="タイトルを入力">
+            <input class="create-form__item-input" type="text" name="content" placeholder="内容を入力"> <!-- 🔹 追加 -->
         </div>
         <div class="create-form__button">
             <button class="create-form__button-submit" type="submit">作成</button>
         </div>
     </form>
+
     <div class="todo-table">
         <table class="todo-table__inner">
             <tr class="todo-table__row">
                 <th class="todo-table__header">Todo</th>
+                <th class="todo-table__header">内容</th> <!-- 🔹 追加 -->
+                <th class="todo-table__header">操作</th>
             </tr>
-            @foreach($todos as $todo)
-            <tr>
-                <td>{{ $todo->title }}</td>
-                <td>
-                    <a href="{{ route('todos.edit', $todo->id) }}" class="btn btn-primary btn-sm">更新</a>
-                    <form action="{{ route('todos.destroy', $todo->id) }}" method="POST" style="display:inline;">
+
+            @foreach ($todos as $todo)
+            <tr class="todo-table__row">
+                <td class="todo-table__item">
+                    <form class="update-form">
+                        <div class="update-form__item">
+                            <p class="update-form__item-input">{{ $todo->content }}</p> <!-- 🔹 追加 -->
+                        </div>
+                        <div class="update-form__button">
+                            <button class="update-form__button-submit" type="submit">更新</button>
+                        </div>
+                    </form>
+                </td>
+                <td class="todo-table__item">
+                    <form class="delete-form" action="{{ route('todos.destroy', $todo->id) }}" method="POST" style="display:inline;">
                         @csrf
                         @method('DELETE')
-                        <button type="submit" class="btn btn-danger btn-sm">削除</button>
+                        <button type="submit" class="delete-form__button-submit">削除</button>
                     </form>
                 </td>
             </tr>
