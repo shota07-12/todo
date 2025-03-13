cd7256bc463af8b793222a8d3edef3691dc64c39
shota <ya07masho12@gmail.com>
2025-03-13 16:31:36 +0900

    Add store method to TodoController

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -13,4 +13,21 @@ class TodoController extends Controller
         $todos = Todo::all(); // データベースから全てのToDoを取得
         return view('index', compact('todos')); // 'index' に $todos を渡す
     }
+
+    public function store(Request $request)
+    {
+        // バリデーション（必要なら）
+        $request->validate([
+            'title' => 'required|string|max:255',
+            'description' => 'nullable|string',
+        ]);
+
+        // 新しいToDoを作成
+        Todo::create([
+            'title' => $request->input('title'),
+            'description' => $request->input('description'),
+        ]);
+
+        return redirect()->route('todos.index')->with('success', 'ToDoを作成しました！');
+    }
 }
