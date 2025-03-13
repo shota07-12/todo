0e38726ba9c00e1ef6b93b0619e4f9414cd8a583
shota <ya07masho12@gmail.com>
2025-03-13 17:00:06 +0900

    Add edit route for todos

--- src/routes/web.php
+++ src/routes/web.php
@@ -20,3 +20,6 @@ Route::get('/', [TodoController::class, 'index'])->name('todos.index');
 // ToDo のルートを定義
 Route::get('/todos', [TodoController::class, 'index']); // 一覧表示
 Route::post('/todos', [TodoController::class, 'store'])->name('todos.store'); // データ登録
+
+// ✨ 編集ページのルートを追加
+Route::get('/todos/{id}/edit', [TodoController::class, 'edit'])->name('todos.edit');
