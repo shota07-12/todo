6b106f153fee7b79d8a0e06201d0b852a11243c4
shota <ya07masho12@gmail.com>
2025-03-17 19:23:33 +0900

    カテゴリー機能の追加・修正

--- src/app/Http/Controllers/CategoryController.php
+++ src/app/Http/Controllers/CategoryController.php
@@ -16,13 +16,6 @@ class CategoryController extends Controller
     public function store(Request $request)
     {
 
-        $request->validate([
-            'name' => 'required|string|max:255',
-        ]);
-
-        $category = $request->only(['name']);
-        Category::create($category);
-
-        return redirect('/categories')->with('message', 'カテゴリを作成しました');
+        dd($request->all()); // ここでデータを確認
     }
 }
